af5d613091800f4087c2ecf5f6a2b800735aabed
Dalton Rogers <rogersdr1994@gmail.com>
2024-02-28 12:03:35 -0600

    second

--- mysite/blogging/models.py
+++ mysite/blogging/models.py
@@ -1,5 +1,10 @@
+from typing import Any
+from django.contrib import admin
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms.models import ModelMultipleChoiceField
+from django.http.request import HttpRequest
+
 
 # Create your models here.
 class Post(models.Model):
@@ -16,10 +21,25 @@ class Post(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
-    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+    posts = models.ManyToManyField(Post, blank=True, through='PostCategory')
 
     class Meta:
         verbose_name_plural = "Catgories"
 
     def __str__(self):
         return self.name
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class PostCategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        PostCategoryInLine,
+    ]
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
